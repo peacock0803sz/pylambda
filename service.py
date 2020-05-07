8fdb5b49f54dfabe77d0ab48ac7bc934bbd337cf
Peacock <peacock0803sz@gmail.com>
2020-05-07 10:02:02 +0900

    :truck: Move service.py

--- service.py
+++ service.py
@@ -1,4 +1,14 @@
-from pylambda.service import main
+from pylambda.foo import ham
+
 
 def lambda_handler(event, context):
-    main(event, context)
+    print(f'Starting Lambda ... {event}, {context}')
+    try:
+        ham()
+    except Exception as e:
+        print(f'Function failed: {e}')
+        raise
+    else:
+        print('Check passed.')
+    finally:
+        print('Finished Lambda Function')
